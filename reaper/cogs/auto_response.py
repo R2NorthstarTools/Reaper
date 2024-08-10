f2ff27c260b74109339b577eb9687ccb2c3f61ce
GeckoEidechse <40122905+GeckoEidechse@users.noreply.github.com>
2024-08-11 01:36:45 +0200

    fix: Define logging configuration in every source file (#15)
    For some reason the logging configuration is not inherited.
    This approach is ugly but works for now.

--- reaper/cogs/auto_response.py
+++ reaper/cogs/auto_response.py
@@ -1,6 +1,10 @@
 import logging
+import coloredlogs
 
 logger = logging.getLogger(__name__)
+coloredlogs.install(
+    level="DEBUG", logger=logger, fmt="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 import datetime
 import discord
