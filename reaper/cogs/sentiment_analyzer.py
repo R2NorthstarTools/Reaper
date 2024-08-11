98ea10e01aa05d2e0d9d80e5bcda9504bfa75f00
Bobbyperson12 <bobbyperson12345@gmail.com>
2024-08-10 21:24:51 -0400

    sentiment analyzer first version

--- reaper/cogs/sentiment_analyzer.py
+++ reaper/cogs/sentiment_analyzer.py
@@ -1,37 +1,67 @@
-import discord
 from transformers import pipeline
 from discord.ext import commands
+import logging
+import asyncio
+import random
+from util import globals
 
 
 class SentimentAnalyzer(commands.Cog):
+    
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+        self.sentiment = pipeline(
+            "sentiment-analysis",
+            model="cardiffnlp/twitter-roberta-base-sentiment-latest",
+            tokenizer="cardiffnlp/twitter-roberta-base-sentiment-latest",
+        )
+        self.reset_message_cache()
+            
+    def reset_message_cache(self):
+        self.bot.cached_sentiment = {}
+        for channel in globals.config["channels"]["sentiment-analysis-channels"]:
+            self.bot.cached_sentiment[channel] = {
+                "positive": 0,
+                "negative": 0,
+                "neutral": 0
+            }
+        
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
             return
-        sentiments = [
-            pipeline(
-                "sentiment-analysis",
-                model="finiteautomata/bertweet-base-sentiment-analysis",
-                tokenizer="finiteautomata/bertweet-base-sentiment-analysis",
-            ),
-            pipeline(
-                "sentiment-analysis",
-                model="cardiffnlp/twitter-roberta-base-sentiment-latest",
-                tokenizer="cardiffnlp/twitter-roberta-base-sentiment-latest",
-            ),
-            pipeline(
-                "sentiment-analysis",
-                model="avichr/heBERT_sentiment_analysis",
-                tokenizer="avichr/heBERT_sentiment_analysis",
-            )
-        ]
-        for sentiment in sentiments:
-            try:
-                await message.channel.send(sentiment(message.content))
-            except:
-                await message.channel.send("Message too big for model.")
-
+        if not message.content:
+            return
+        if message.channel.id not in globals.config["channels"]["sentiment-analysis-channels"]:
+            return
+        msg_ratio = globals.config["general"]["sentiment-analysis-ratio"]
+        if random.randint(1, msg_ratio) != 1:
+            return
+        
+        
+        try:
+            result = await asyncio.to_thread(self.sentiment, message.content) # need to do this otherwise the bot will not do anything until this is done
+        except Exception as e:
+            logging.error(f"Error in sentiment analyzer: {e}")
+            return
+        
+        self.bot.cached_sentiment[message.channel.id][result[0]["label"]] += 1
+        
+        total_negative = self.bot.cached_sentiment[message.channel.id]["negative"]
+        total_normal = self.bot.cached_sentiment[message.channel.id]["neutral"] + self.bot.cached_sentiment[message.channel.id]["positive"]
+        
+        total = total_negative + total_normal
+        
+        messages_to_cache = globals.config["general"]["sentiment-analysis-cache"]
+        
+        if total >= messages_to_cache:
+            if total_negative > total_normal:
+                moderator_channel = self.bot.get_channel(
+                    globals.config["channels"]["sentiment-analysis-warn-channel"]
+                )
+                await moderator_channel.send(f"Negative sentiment detected in {message.channel.mention}! Out of the last {messages_to_cache} messages, {total_negative} were negative.")
+            self.reset_message_cache()
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(SentimentAnalyzer(bot))
