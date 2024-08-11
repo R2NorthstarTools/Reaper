421bc4ea1aa12d412d6b32755d9d060a6484839f
Bobbyperson12 <bobbyperson12345@gmail.com>
2024-08-10 21:33:43 -0400

    forgot to format with black whoops

--- reaper/cogs/sentiment_analyzer.py
+++ reaper/cogs/sentiment_analyzer.py
@@ -7,7 +7,7 @@ from util import globals
 
 
 class SentimentAnalyzer(commands.Cog):
-    
+
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
         self.sentiment = pipeline(
@@ -16,16 +16,15 @@ class SentimentAnalyzer(commands.Cog):
             tokenizer="cardiffnlp/twitter-roberta-base-sentiment-latest",
         )
         self.reset_message_cache()
-            
+
     def reset_message_cache(self):
         self.bot.cached_sentiment = {}
         for channel in globals.config["channels"]["sentiment-analysis-channels"]:
             self.bot.cached_sentiment[channel] = {
                 "positive": 0,
                 "negative": 0,
-                "neutral": 0
+                "neutral": 0,
             }
-        
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -33,35 +32,45 @@ class SentimentAnalyzer(commands.Cog):
             return
         if not message.content:
             return
-        if message.channel.id not in globals.config["channels"]["sentiment-analysis-channels"]:
+        if (
+            message.channel.id
+            not in globals.config["channels"]["sentiment-analysis-channels"]
+        ):
             return
         msg_ratio = globals.config["general"]["sentiment-analysis-ratio"]
         if random.randint(1, msg_ratio) != 1:
             return
-        
-        
+
         try:
-            result = await asyncio.to_thread(self.sentiment, message.content) # need to do this otherwise the bot will not do anything until this is done
+            result = await asyncio.to_thread(
+                self.sentiment, message.content
+            )  # need to do this otherwise the bot will not do anything until this is done
         except Exception as e:
             logging.error(f"Error in sentiment analyzer: {e}")
             return
-        
+
         self.bot.cached_sentiment[message.channel.id][result[0]["label"]] += 1
-        
+
         total_negative = self.bot.cached_sentiment[message.channel.id]["negative"]
-        total_normal = self.bot.cached_sentiment[message.channel.id]["neutral"] + self.bot.cached_sentiment[message.channel.id]["positive"]
-        
+        total_normal = (
+            self.bot.cached_sentiment[message.channel.id]["neutral"]
+            + self.bot.cached_sentiment[message.channel.id]["positive"]
+        )
+
         total = total_negative + total_normal
-        
+
         messages_to_cache = globals.config["general"]["sentiment-analysis-cache"]
-        
+
         if total >= messages_to_cache:
             if total_negative > total_normal:
                 moderator_channel = self.bot.get_channel(
                     globals.config["channels"]["sentiment-analysis-warn-channel"]
                 )
-                await moderator_channel.send(f"Negative sentiment detected in {message.channel.mention}! Out of the last {messages_to_cache} messages, {total_negative} were negative.")
+                await moderator_channel.send(
+                    f"Negative sentiment detected in {message.channel.mention}! Out of the last {messages_to_cache} messages, {total_negative} were negative."
+                )
             self.reset_message_cache()
+
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(SentimentAnalyzer(bot))
