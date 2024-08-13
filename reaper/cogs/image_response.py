6fb77f3797da93bbe2c2950878f52819e89af384
GeckoEidechse <gecko.eidechse+git@pm.me>
2024-08-13 17:28:15 +0200

    style: Change variable naming to snake_case

--- reaper/cogs/image_response.py
+++ reaper/cogs/image_response.py
@@ -17,38 +17,38 @@ import discord
 
 information_message = "Please note that I'm a bot automatically reading your image. There is a chance this information is wrong, in which case please ping @geckoeidechse"
 
-vanillaPlus = discord.Embed(
+vanilla_plus = discord.Embed(
     description="If you're using VanillaPlus, please make sure to read the instructions [on the mod page](https://northstar.thunderstore.io/package/NanohmProtogen/VanillaPlus/) (if you're updating from an older version, the steps have changed, so please read it again)\n\nIf you AREN'T using VanillaPlus, make sure you didn't disable any core mods. You can see if they're enabled or disabled in `R2Northstar/enabledmods.json`. Either change `false` to `true` on disabled core mods, or delete `enabledmods.json` and ALL mods will automatically enable",
     color=0x5D3FD3,
 )
-vanillaPlus.add_field(
+vanilla_plus.add_field(
     name="",
     value=information_message,
 )
 
-operationNotPermitted = discord.Embed(
+operation_not_permitted = discord.Embed(
     description="EA's default install directory has some issues associated with it, which can be solved by following the [wiki section about this error](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#cannot-write-log-file-when-using-northstar-on-ea-app)",
     color=0x5D3FD3,
 )
-operationNotPermitted.add_field(
+operation_not_permitted.add_field(
     name="",
     value=information_message,
 )
 
-modFailedSanity = discord.Embed(
+mod_failed_sanity = discord.Embed(
     description="The \"Mod failed sanity check\" error is specific to FlightCore, and means that the mod isn't properly formatted and FlightCore can't automatically install it. However, you can still follow the [manual mod install guide](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/manual-installation#installing-northstar-mods-manually) to install the mod you wanted.",
     color=0x5D3FD3,
 )
-modFailedSanity.add_field(
+mod_failed_sanity.add_field(
     name="",
     value=information_message,
 )
 
-logFile = discord.Embed(
+log_file = discord.Embed(
     description='I noticed you encountered the "Failed creating log file!" error.\n\nPlease follow the [wiki section](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#cannot-write-log-file-when-using-northstar-on-ea-app) for solving this issue.',
     color=0x5D3FD3,
 )
-logFile.add_field(
+log_file.add_field(
     name="",
     value=information_message,
 )
@@ -71,29 +71,29 @@ playeraccount.add_field(
     value=information_message,
 )
 
-originOffline = discord.Embed(
+origin_offline = discord.Embed(
     description='Try following the guide on solving the "Origin Offline" and "Origin logged out" errors [here](https://r2northstar.gitbook.io/r2northstar-wiki/installing-northstar/troubleshooting#origin-offline)',
     color=0x5D3FD3,
 )
-originOffline.add_field(
+origin_offline.add_field(
     name="",
     value=information_message,
 )
 
-scriptComp = discord.Embed(
+script_comp = discord.Embed(
     description="From this image alone, we can't see what's causing the issue. Please send a screenshot of the console (open it by hitting the `~` key), or send the newest log. You can find logs in `Titanfall2/R2Northstar/logs`, with the newest being on the bottom by default.",
     color=0x5D3FD3,
 )
-scriptComp.add_field(
+script_comp.add_field(
     name="",
     value=information_message,
 )
 
-engineError = discord.Embed(
+engine_error = discord.Embed(
     description="Found a screenshot showing 'Engine Error'. Uploading a log file might be useful to resolve this issue",
     color=0x5D3FD3,
 )
-engineError.add_field(
+engine_error.add_field(
     name="",
     value=information_message,
 )
@@ -103,7 +103,7 @@ async def handle_response(text: str, message):
     if re.search(
         "encountered.client.script.compilation.error", text.lower()
     ) and re.search("error|help", message.content.lower()):
-        await message.channel.send(embed=scriptComp, reference=message)
+        await message.channel.send(embed=script_comp, reference=message)
         return
 
     if re.search("invalid.or.expired.masterserver.token", text.lower()) or re.search(
@@ -115,7 +115,7 @@ async def handle_response(text: str, message):
     if re.search("origin.offline", text.lower()) or re.search(
         "origin_logged_out", text.lower()
     ):
-        await message.channel.send(embed=originOffline, reference=message)
+        await message.channel.send(embed=origin_offline, reference=message)
         return
 
     if re.search("MSVCP120|MSVCR120", text.lower()):
@@ -123,29 +123,29 @@ async def handle_response(text: str, message):
         return
 
     if re.search("failed.creating.log.file", text.lower()):
-        await message.channel.send(embed=logFile, reference=message)
+        await message.channel.send(embed=log_file, reference=message)
         return
 
     # FlightCore specific errors
     if re.search("mod.failed.sanity.check", text.lower()):
-        await message.channel.send(embed=modFailedSanity, reference=message)
+        await message.channel.send(embed=mod_failed_sanity, reference=message)
         return
 
     # Viper specific errors
     if re.search("operation.not.permitted", text.lower()) and re.search(
         "ea.games", text.lower()
     ):
-        await message.channel.send(embed=operationNotPermitted, reference=message)
+        await message.channel.send(embed=operation_not_permitted, reference=message)
         return
 
     if re.search("compile.error.undefined.variable", text.lower()) and re.search(
         "progression_getpreference", text.lower()
     ):
-        await message.channel.send(embed=vanillaPlus, reference=message)
+        await message.channel.send(embed=vanilla_plus, reference=message)
         return
 
     if re.search("engine.error", text.lower()):
-        await message.channel.send(embed=engineError, reference=message)
+        await message.channel.send(embed=engine_error, reference=message)
         return
 
 
