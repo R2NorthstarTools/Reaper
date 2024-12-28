322b70c222aa9d360d8b73ac58abeb4b4433ec81
Bobbyperson <bobbyperson12345@gmail.com>
2024-12-27 22:47:55 -0500

    add docsearch command

---
+++ reaper/cogs/doc_search.py
@@ -0,0 +1,18 @@
+from discord.ext import commands
+import urllib
+
+
+class DocSearch(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    @commands.command()
+    async def docsearch(self, ctx, *, query):
+        query = query.replace(" ", "+")
+        # convert special characters to link friendly format
+        urllib.parse.quote_plus(query)
+        await ctx.send(f"https://docs.northstar.tf/?q={query}")
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(DocSearch(bot))
