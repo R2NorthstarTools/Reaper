18a7b1959e0fc7bdd105166178d9223b08b75412
GeckoEidechse <gecko.eidechse+git@pm.me>
2025-04-17 21:47:13 +0200

    fix: Add missing `__init__.py`

---
+++ reaper/cogs/__init__.py
@@ -0,0 +1,6 @@
+"""
+Contains the different cogs used by the bot.
+
+Cogs are a way to organise commands into seperate classes and modules to allow the bot to be modular.
+These cogs can be turned on and off, and hot-reloaded.
+"""
